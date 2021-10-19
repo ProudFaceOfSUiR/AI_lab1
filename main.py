c5f7420efb4416b0c8b93940dda074b41179090b
Anatoly <anatolyskier@gmail.com>
2021-10-19 14:27:33 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+import os
+import numpy as np
+import pandas as pd
+from sklearn import svm
+from sklearn import svr
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
+
+
+def read_data(filename): #C:\Users\Anatoly\PycharmProjects\AI_lab1
+    return pd.read_csv(os.path.join(filename))
+
+def load_dataset(label_dict):
+    train_X = read_data('train.csv').values[:,:-2]
+    train_y = read_data('train.csv')['Activity']
+    train_y = train_y.map(label_dict).values
+    test_X = read_data('test.csv').values[:,:-2]
+    test_y = read_data('test.csv')
+    test_y = test_y['Activity'].map(label_dict).values
+    return(train_X, train_y, test_X, test_y)
+
+
+label_dict = {'WALKING':0, 'WALKING_UPSTAIRS':1, 'WALKING_DOWNSTAIRS':2, 'SITTING':3, 'STANDING':4, 'LAYING':5}
+
+
+train_X, train_Y, test_X, test_Y = load_dataset(label_dict)
+
+SVC_model = svm.SVC()
+# В KNN-модели нужно указать параметр n_neighbors
+# Это число точек, на которое будет смотреть
+# классификатор, чтобы определить, к какому классу принадлежит новая точка
+KNN_model = KNeighborsClassifier(n_neighbors=6)
+SVR_model = svr.SVR()
+
+SVC_model.fit(train_X, train_Y)
+KNN_model.fit(train_X, train_Y)
+
+SVC_prediction = SVC_model.predict(test_X)
+KNN_prediction = KNN_model.predict(test_X)
+
+SVC_yhat = SVC_model.predict(test_X)
+KNN_yhat = KNN_model.predict(test_X)
+
+print("SVC model accuracy = " + str(accuracy_score(SVC_prediction, test_Y)))
+print("KNN model accuracy = " + str(accuracy_score(KNN_prediction, test_Y)))
+
+from sklearn.metrics import classification_report
+target_names = ['Walking', 'Walking Upstairs', 'Walking Downstairs', 'Sitting', 'Standing', 'Laying']
+
+print(classification_report(test_Y, SVC_prediction, target_names=target_names))
+print(classification_report(test_Y, KNN_prediction, target_names=target_names))
